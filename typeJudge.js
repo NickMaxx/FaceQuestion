51a9518653d5cda93daa6066632e780dba81439e
wang_kai <wang_kai@egova.com.cn>
2019-12-30 10:14:40 +0800

    M

--- typeJudge.js
+++ typeJudge.js
@@ -1,6 +1,8 @@
 //typeof 能够准确判断出基本类型，但是null除外，null会返回object
 //typeof 对于对象类型，能够准确辨认出func类型，其余全是object
 
+//typeof 能判断基本类型，instanceof不能判断基本类型，instanceof要借助于Symbol.hasInstance
+
 class isString{
   static [Symbol.hasInstance](x){
     return typeof x === 'string';
