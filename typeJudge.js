431e8e63a448ea84de694d823c2e1fe8d0c7f802
wang_kai <wang_kai@egova.com.cn>
2019-12-30 09:38:49 +0800

    增加类型判断案例

---
+++ typeJudge.js
@@ -0,0 +1,15 @@
+//typeof 能够准确判断出基本类型，但是null除外，null会返回object
+//typeof 对于对象类型，能够准确辨认出func类型，其余全是object
+
+class isString{
+  static [Symbol.hasInstance](x){
+    return typeof x === 'string';
+  }
+}
+
+console.log('a' instanceof isString, 123);
+console.log(1 instanceof isString, 123);
+
+console.log(typeof function a(){},234);
+console.log(typeof {}, 234);
+console.log(typeof null, 234);
